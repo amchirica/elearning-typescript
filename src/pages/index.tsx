b8a9604662cab47789d597e95882f2b9caf9ca7c
zerK4 <sebastian.v.pavel@gmail.com>
2023-04-14 13:48:17 +0300

    fix gradient and text color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,8 +29,10 @@ const Home = () => {
               ></path>
             </svg>
           </Link>
-          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-          <span className="text">#Vino în lumea limbajelor de programare"</span>
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl">
+            <span className="text text-white">
+              #Vino în lumea limbajelor de programare
+            </span>
           </h1>
           <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">
             La platforma noastră de e-learning pentru programare, ne concentrăm
@@ -76,7 +78,7 @@ const Home = () => {
         <div className="bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
       </div>
 
-      <div className="flex flex-wrap gap-3 justify-center">
+      <div className="flex flex-wrap gap-3 justify-center z-40">
         <div className="max-w-sm p-20 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
           <Link href="/courses">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -124,7 +126,7 @@ const Home = () => {
             href="/courses"
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Read more
+            <button>Read more</button>
             <svg
               aria-hidden="true"
               className="w-4 h-4 ml-2 -mr-1"
